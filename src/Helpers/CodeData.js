c82272d2eca3ea6d4d20fdd07320920ad0d7c582
jedtap <doom.slaver2@gmail.com>
2023-02-08 20:56:27 +0800

    new project, updated resume, updated hero name

--- src/Helpers/CodeData.js
+++ src/Helpers/CodeData.js
@@ -6,7 +6,7 @@ const CodeData = [
   { key: 105, name: 'Ruby', img: 'devicon-ruby-plain colored' },
   { key: 106, name: 'Bootstrap', img: 'devicon-bootstrap-plain colored' },
   { key: 107, name: 'React', img: 'devicon-react-original colored' },
-  { key: 108, name: 'Rails', img: 'devicon-rails-plain colored' },
+  { key: 108, name: 'Rails 7', img: 'devicon-rails-plain colored' },
   { key: 109, name: 'Jest', img: 'devicon-jest-plain colored' },
   { key: 110, name: 'TailwindCSS', img: 'devicon-tailwindcss-plain colored' },
 ];
